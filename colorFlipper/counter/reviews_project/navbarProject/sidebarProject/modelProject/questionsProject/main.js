3b8c274cc4621f4722250124b41a5c6033312041
Admin <admin@Admins-MacBook-Air.local>
2022-08-17 14:35:26 +0530

    added menu project

---
+++ colorFlipper/counter/reviews_project/navbarProject/sidebarProject/modelProject/questionsProject/main.js
@@ -0,0 +1,19 @@
+// using selectors inside the element
+// traversing dom
+
+const questions=document.querySelectorAll('.question')
+
+questions.forEach(function (question) {
+   const btn=question.querySelector('.question-btn') 
+
+   btn.addEventListener('click', function(){
+       console.log(question)
+
+       questions.forEach(function (item){
+           if(item!==question){
+               item.classList.remove('show-text')
+           }
+       })
+       question.classList.toggle('show-text')
+   })
+})
